d5a8dbe04965bfa0e8a359f7011a77f82c498a4e
Elizabeth Saka <esaka28@andover.edu>
2025-08-06 15:12:10 -0400

    Im trying to add a "Type Your Birthday" box but it's lowkey not working... Fix changes later.

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -10,19 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var name = ""
     @State private var textTitle = "What is your name?"
+    @State private var birthday = ""
+    @State private var textBox = "When is Your Birthday?"
     var body: some View {
         VStack {
             Text (textTitle)
                 .font(.title)
                 .fontWeight(.bold)
             TextField("Type Your Name Here...", text: $name)
+                .padding(.bottom)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
                 textTitle = "Welcome, \(name)!"
                 name = ""
+            
+        // Name Box
+                
+            TextField("Type Your Birthday Here...", text: $birthday)
+                .padding(.bottom)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                textBox = "Welcome, \(name)!"
+                birthday = ""
             }
+            .padding(.top)
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
