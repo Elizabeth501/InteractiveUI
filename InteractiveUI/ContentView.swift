df92f4c9c7687dc2ec48928b60fc2f375cdb9b0f
Elizabeth Saka <esaka28@andover.edu>
2025-08-06 14:38:53 -0400

    Added a textfeild, modiffiers and button.

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -13,6 +13,16 @@ struct ContentView: View {
             Text ("What is Your Name?")
                 .font(.title)
                 .fontWeight(.bold)
+            TextField("Type Your Name Here...", text: Value)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
