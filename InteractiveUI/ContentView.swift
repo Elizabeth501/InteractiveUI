96ee103e86de3525cac2b19eee96ccf8d5cff564
Elizabeth Saka <esaka28@andover.edu>
2025-08-06 14:45:54 -0400

    Fixed what was broken and allowed user to interact with text and button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
     var body: some View {
         VStack {
             Text ("What is Your Name?")
                 .font(.title)
                 .fontWeight(.bold)
-            TextField("Type Your Name Here...", text: Value)
+            TextField("Type Your Name Here...", text: $name )
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                print(name)
             }
             font(.title2)
             .buttonStyle(.borderedProminent)
